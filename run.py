f4c3c13d43e6ba431ae83a77a13ab91277d02c91
Tarik M <G-Tarik@users.noreply.github.com>
2018-07-26 18:41:58 +0200

    load db config

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 from app import create_app, socketio
+import app_config, db_config
 
 if __name__ == '__main__':
-    app =  create_app('config.DevConfig')
+    app =  create_app(app_config.DevConfig, db_config.PGSQL)
     socketio.run(app, app.config['HOST'], app.config['PORT'])
