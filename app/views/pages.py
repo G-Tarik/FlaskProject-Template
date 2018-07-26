9837504520eb85a601e0e7d3b4cf2190c6bde812
Tarik M <G-Tarik@users.noreply.github.com>
2018-07-26 18:51:56 +0200

    query database

--- app/views/pages.py
+++ app/views/pages.py
@@ -1,4 +1,5 @@
 from flask import render_template, flash, Blueprint
+from app.models import db, Users
 
 blue_view = Blueprint('views', __name__)
 
@@ -7,7 +8,8 @@ blue_view = Blueprint('views', __name__)
 def index():
     info_message = 'Hello magic :-)'
     flash(info_message)
-    return render_template('index.html')
+    users = db.session.query(Users).all()
+    return render_template('index.html', users=users)
 
 
 @blue_view.route('/about')
