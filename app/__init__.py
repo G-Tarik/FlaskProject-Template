7a1cd94da8759e3462a3571d0e2c775d662c29f5
Tarik M <G-Tarik@users.noreply.github.com>
2018-07-26 18:46:19 +0200

    add initialization of db

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,17 @@ from flask_socketio import SocketIO
 
 socketio = SocketIO()
 
-def create_app(config):
+def create_app(app_config, db_config):
     app = Flask(__name__)
-    app.config.from_object(config)
-    app.config.update(SECRET_KEY=urandom(16))
+    app.config.from_object(app_config)
+    app.config.update(SECRET_KEY=urandom(16),
+                      SQLALCHEMY_DATABASE_URI=db_config.URI)
 
     from .views.pages import blue_view
     app.register_blueprint(blue_view)
+
+    from app.models import db
+    db.init_app(app)
 
     socketio.init_app(app, logger=True, engineio_logger=True)
     return app
