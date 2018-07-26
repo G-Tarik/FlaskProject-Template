c892df3cf2217417299c0e80f2fd9b454d6d7426
Tarik M <G-Tarik@users.noreply.github.com>
2018-07-26 08:34:42 +0200

    Generate SECRET_KEY using os.urandom

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-
+from os import urandom
 from flask import Flask
 from flask_socketio import SocketIO
 
 socketio = SocketIO()
 
-def create_app(config_file):
+def create_app(config):
     app = Flask(__name__)
-    app.config.from_object(config_file)
+    app.config.from_object(config)
+    app.config.update(SECRET_KEY=urandom(16))
+
     from .views.pages import blue_view
     app.register_blueprint(blue_view)
 
